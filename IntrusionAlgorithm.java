5b46d516a9406fdedf27b5b8deacac2e218e4295
ZackJozwiak <zjozwiak@att.net>
2016-12-05 17:48:01 -0600

    Delete IntrusionAlgorithm.java

--- IntrusionAlgorithm.java
+++
@@ -1,8 +0,0 @@
-
-public interface IntrusionAlgorithm 
-{
-	public abstract String[] getParameterNames();
-	public abstract void setParameters(double[] values);
-	public abstract double[] doNextMove(double x, double y);
-	public abstract void resetAlg();
-}
